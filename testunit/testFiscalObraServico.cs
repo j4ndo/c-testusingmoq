50e61a5740943a1edc8d442a6d8eeef1e81097f7
Janderson Fonseca <janderson-fonseca@hotmail.com>
2020-07-06 21:50:53 -0300

    testing units using moq

--- testunit/testFiscalObraServico.cs
+++ testunit/testFiscalObraServico.cs
@@ -4,25 +4,30 @@ using NUnit.Framework;
 using unit.entities;
 using unit.services;
 
-namespace testunit {
-  public class TestFiscalObraServico {
+namespace testunit
+{
+  public class TestFiscalObraServico
+  {
     private FiscalObraServico _fiscalObraServico;
 
     [SetUp]
-    public void Setup () {            
+    public void Setup()
+    {
     }
 
     [TestFixture]
-    public class FiscalObraTests{      
+    public class FiscalObraTests
+    {
       Mock<IFiscalObraPeriodoService> mockFiscalObraPeriodoService;
       FiscalObraServico fiscalObraServico;
 
       [TestCase("12345678910", false)]
       [TestCase("12345678911", true)]
-      [TestCase("70098534440", false)]
+      [TestCase("45698345005", false)]
       [TestCase("12345678992", true)]
       [TestCase("45638542010", false)]
-      public void testeCadastroDuplicadoFiscalObra(string CPF, bool expectedResult){
+      public void testeCadastroDuplicadoFiscalObra(string CPF, bool expectedResult)
+      {
         mockFiscalObraPeriodoService = new Mock<IFiscalObraPeriodoService>(MockBehavior.Strict);
         mockFiscalObraPeriodoService
         .Setup(p => p.verificaCadastroDuplicado(CPF))
@@ -34,9 +39,9 @@ namespace testunit {
 
         Assert.That(result, Is.EqualTo(expectedResult));
 
-        mockFiscalObraPeriodoService.VerifyAll(); 
+        mockFiscalObraPeriodoService.VerifyAll();
       }
 
-    }    
+    }
   }
 }
